073538e7a45a9188e41da19501e55000717015cc
luphord <luphord@protonmail.com>
2019-06-25 06:56:37 +0200

    cleanup and happy linter

--- tests/test_user_voting_rocks.py
+++ tests/test_user_voting_rocks.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Tests for `user_voting_rocks` package."""
+'''Tests for `user_voting_rocks` package.'''
 
 
 import unittest
 from click.testing import CliRunner
 
-from user_voting_rocks import user_voting_rocks
 from user_voting_rocks import cli
 
 
 class TestUser_voting_rocks(unittest.TestCase):
-    """Tests for `user_voting_rocks` package."""
+    '''Tests for `user_voting_rocks` package.'''
 
     def setUp(self):
-        """Set up test fixtures, if any."""
+        '''Set up test fixtures, if any.'''
 
     def tearDown(self):
-        """Tear down test fixtures, if any."""
+        '''Tear down test fixtures, if any.'''
 
     def test_000_something(self):
-        """Test something."""
+        '''Test something.'''
 
     def test_command_line_interface(self):
-        """Test the CLI."""
+        '''Test the CLI.'''
         runner = CliRunner()
         result = runner.invoke(cli.main)
         assert result.exit_code == 0
